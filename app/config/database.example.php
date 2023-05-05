02a9916e12c0c1380c66ae08444367eb278fab51
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-05-05 12:46:32 +0800

    Prepare for Queen of the Universe (San Nicolas, Iriga City).

--- app/config/database.example.php
+++ app/config/database.example.php
@@ -17,7 +17,7 @@ $config = [
     'host'   => 'localhost',
     'user'   => 'root',
     'pass'   => '',
-    'dbname' => 'bbsv-buhi'
+    'dbname' => 'qotu.sn-iriga'
 ];
 
 $conn = new mysqli($config['host'], $config['user'], $config['pass'], $config['dbname']);
