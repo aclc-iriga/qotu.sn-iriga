8268b6ef0b1a52260d58e954290f330ed9710798
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-05-25 19:49:31 +0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -39,7 +39,7 @@ export default createStore({
         // get app url
         appURL(state) {
             const location = window.location;
-            if(location.hostname === 'localhost' && location.port === '5178')
+            if(location.hostname === 'localhost' && location.port === '5182')
                 return `http://localhost${import.meta.env.BASE_URL}${state.app.backendDir}`;
             else
                 return `${location.protocol}//${location.hostname}${import.meta.env.BASE_URL}${state.app.backendDir}`;
